ca92d337375d82f529e96d681d8c3874dd456a0b
ntua-el18939 <el18939@mail.ntua.gr>
2025-01-02 17:18:30 +0200

    authentication bug fix

--- back-end/middleware/auth.js
+++ back-end/middleware/auth.js
@@ -1,11 +1,11 @@
+const jwt = require('jsonwebtoken');
+
 const authenticate = (req, res, next) => {
-  const token = req.headers['x-observatory-auth']; // Custom header
+const token = req.headers['x-observatory-auth']; // Custom header
 
   if (!token) return res.status(401).json({ message: 'Unauthorized' });
 
   try {
-    console.log(token);
-    console.log(process.env.JWT_SECRET);
     const decoded = jwt.verify(token, process.env.JWT_SECRET); // Use your secret key
     req.user = decoded; // Attach decoded token payload to the request
     next();
