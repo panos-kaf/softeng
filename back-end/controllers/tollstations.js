b252d131f410ef3a6a5aeab80b89e8e014c0c560
el21721 <el21721@mail.ntua.gr>
2025-02-08 18:27:39 +0200

    tollStations api bug fixed

--- back-end/controllers/tollstations.js
+++ back-end/controllers/tollstations.js
@@ -2,9 +2,9 @@ const db = require("../utils/db");
 
 exports.getTollStations = async (req, res) => {
     try {
-        const operator_id = req.user.operator_id; // Παίρνουμε το ID του operator από το token
+        const operator_name = req.user.operator_name; // Παίρνουμε το ID του operator από το token
 
-        if (!operator_id) {
+        if (!operator_name) {
             return res.status(403).json({ message: "Δεν έχετε πρόσβαση σε σταθμούς" });
         }
 
@@ -13,8 +13,8 @@ exports.getTollStations = async (req, res) => {
         let params = [];
 
         if (req.user.role !== "admin") {
-            query += " WHERE operator_id = ?";
-            params = [operator_id];
+            query += " WHERE name = ?";
+            params = [operator_name];
         }
 
         const [stations] = await db.query(query, params);
