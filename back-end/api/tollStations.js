530d3ed4fab3d0062c7f8548a8dbc1b3fb8419c7
ntua-el18939 <el18939@mail.ntua.gr>
2025-02-08 18:08:08 +0200

    bugfix

--- back-end/api/tollStations.js
+++ back-end/api/tollStations.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const authenticate = require("../middleware/auth"); // Προστασία με JWT token
-const { tollStations } = require("../controllers/tollstations");
+const tollStationsController = require("../controllers/tollStations");
 
 const router = express.Router();
 
 // Endpoint: GET /api/tollStations
-router.get("/", authenticate, tollStations);
+router.get("/", authenticate, tollStationsController.getTollStations);
 
 module.exports = router;
