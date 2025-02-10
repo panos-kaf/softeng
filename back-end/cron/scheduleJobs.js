e07b2a51d920cd063210ba77dc1c3a9c2b779457
ntua-el18939 <el18939@mail.ntua.gr>
2025-02-11 00:32:04 +0200

    runs saveSettlements monthly

--- back-end/cron/scheduleJobs.js
+++ back-end/cron/scheduleJobs.js
@@ -1,9 +1,7 @@
 const cron = require('node-cron');
-const processPayments = require('../utils/processPayments');
+const saveSettlements = require('../utils/saveSettlements');
 
-var i = 0;
-
-cron.schedule('0 * * * *', async () => { // Min Hour Day Month Year //
-    console.log(`#${i} Running daily payment settlement...`);
-    await processPayments(i++);
+cron.schedule('0 0 1 * *', async () => { // Min Hour Day Month Year //
+    console.log(`Saving monthly settlements to database...`);
+    await saveSettlements();
 });
